fed8b01184a5dbcec0beb00af581c8cf2436337a
migo0127 <migo0127@gmail.com>
2023-04-05 18:00:01 +0800

    feat: mixed chart

--- src/app/pages/component/dashboard/dashboardomponent.ts
+++ src/app/pages/component/dashboard/dashboardomponent.ts
@@ -10,6 +10,7 @@ import { AuthService, UserInfoService,  } from 'src/app/service';
 })
 export class DashboardComponent implements OnInit {
 
+  mixedChartCongif: any;
   barChartConfig: any;
   barChartConfig2: any;
 
@@ -25,32 +26,43 @@ export class DashboardComponent implements OnInit {
   }
 
   initChartData(): void {
-    this.barChartConfig = {
-      type: CHARTJSTYPE.BAR,
 
-      data: {
-        labels: ['一月', '二月', '三月'],
-        datasets: [{
-          label: '銷售業績(百萬)',
-          data: [60, 49, 72]
-        }]
-      },
-      options: {
-        maintainAspectRatio: 0,
-      }
-    }
-
-    this.barChartConfig2 = {
+    this.mixedChartCongif = {
       type: CHARTJSTYPE.BAR,
       data: {
-        labels: ['四月', '五月', '六月'],
-        datasets: [{
-          label: '銷售業績(百萬)',
-          data: [70, 59, 92]
-        }]
+        labels: ["2023/04/01", "", "2023/04/03", "", "2023/04/05", ""],
+        datasets: [
+          {
+            type: "bar",
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+            label: "註冊數",
+            data: [60, 49, 72, 90, 100, 60]
+          },
+          {
+            type: "line",
+            label: "銷售量",
+            data: [25, 13, 30, 35, 25, 40],
+            lineTension: 0, // 曲線的彎度，設 0 表示直線
+            fill: true // 是否填滿色彩
+          }
+        ]
       },
       options: {
-        maintainAspectRatio: 0,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+        scales: {
+          yAxes: [{
+            id: 'first-y-axis',
+            type: 'linear'
+          }, {
+            id: 'second-y-axis',
+            type: 'linear'
+          }]
+        }
       }
     }
   }
