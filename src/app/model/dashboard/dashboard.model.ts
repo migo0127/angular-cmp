c462c0d615535d0fd7eefc26ce99d3c3eda7ea05
migo0127 <migo0127@gmail.com>
2023-04-08 03:31:54 +0800

    feat: add model

--- src/app/model/dashboard/dashboard.model.ts
+++ src/app/model/dashboard/dashboard.model.ts
@@ -1,7 +1,19 @@
+export interface CostData {
+  type: string;
+  icon: string;
+  current: number;
+  last: number;
+}
+
 export interface ProductDashboard {
   index: number;
   name: string;
   price: number;
   qty: number;
   total: number;
+}
+
+export interface MarketingData {
+  service: string[];
+  network: string[]
 }
