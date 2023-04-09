5060515ac40e9b9027eda4b2ad3cea5648fdcbc3
migo0127 <migo0127@gmail.com>
2023-04-09 23:30:22 +0800

    feat(mobile-side-nav): complete

--- src/app/pages/component/mobile-side-nav/mobile-side-nav.component.ts
+++ src/app/pages/component/mobile-side-nav/mobile-side-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SideNavList } from 'src/app/model';
+import { SideNavService } from 'src/app/service';
 
 @Component({
   selector: 'app-mobile-side-nav',
@@ -7,13 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileSideNavComponent implements OnInit {
 
-  constructor() { }
+  sideNavList: SideNavList[];
+
+  constructor(
+    private router: Router,
+    private sideNavService: SideNavService,
+  ) { }
 
   ngOnInit(): void {
+    this.sideNavService.getSideNavList().subscribe((list: SideNavList[]) => {
+      this.sideNavList = list;
+    });
   }
 
-  onClickMobileNav(): void {
-    console.log(11111);
+  navigateTo(list: SideNavList, e: Event): void{
+    if(!list.url && !list.children){
+      return;
+    }else if(!list.url && list.children){
+      e.stopPropagation();
+      list.visible = !list.visible;
+    }else{
+      this.router.navigate([`${ list.url }`]);
+    }
   }
 
 }
