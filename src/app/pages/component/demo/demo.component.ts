2b03a6ca41620a110986d982e909212bec246a0d
migo0127 <migo0127@gmail.com>
2023-04-12 23:58:00 +0800

    feat(demo): progress 改成倒數計時器

--- src/app/pages/component/demo/demo.component.ts
+++ src/app/pages/component/demo/demo.component.ts
@@ -1,37 +1,69 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
-import { Observable, map, startWith, takeWhile, tap, timer } from 'rxjs';
+import { Observable, Subscription, map, takeWhile, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DemoComponent implements OnInit {
 
   date: string | string[] | null = null;
 
   progressMode: ProgressBarMode = 'determinate';
-  progressValue: number = 0;
-  progress$: Observable<number>;
+  progressValue: number = 100;
+  countdownTime: number = 600;
+  countdownTimer$: Observable<number>;
+  countdownTimerSubscription: Subscription;
+  minutes: number = 10;
+  seconds: number = 0;
 
-  constructor(){ }
+  // progressValue: number = 100;
+  // progress$: Observable<number>;
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+  ){ }
 
   ngOnInit(): void {
-    this.progress$ = this.initProgress();
+    this.countdownTimer$ = timer(0, 1000).pipe(
+      map((i) => {
+        this.progressValue -= 0.1667;
+        return this.countdownTime - i;
+      }),
+      tap((time: number) => console.log(time)),
+      takeWhile((time: number) => time >= 0)
+    );
+    // this.progress$ = this.initProgress();
   }
 
-  initProgress(): Observable<number> {
-    return timer(0, 1000).pipe(
-      startWith(0),
-      map( v => v + 1),
-      takeWhile(v => v <= 10),
-      tap( v => {
-        this.progressValue = v;
-        return v;
-      }),
-    );
+  // initProgress(): Observable<number> {
+  //   return timer(0, 1000).pipe(
+  //     startWith(0),
+  //     map( v => v + 1),
+  //     takeWhile(v => v <= 10),
+  //     tap( v => {
+  //       this.progressValue = v;
+  //       return v;
+  //     }),
+  //   );
+  // }
+
+  startCountdownTime(): void {
+    if (this.countdownTimerSubscription) this.stopCountdownTime();
+    this.progressValue = 100;
+    this.countdownTimerSubscription = this.countdownTimer$.subscribe((time: number) => {
+      this.minutes = Math.floor(time / 60);
+      this.seconds = time % 60;
+    });
+  }
+
+  stopCountdownTime(): void {
+    if (this.countdownTimerSubscription) {
+      this.countdownTimerSubscription.unsubscribe();
+    }
   }
 
   onDateChange(e: string | string[]): void {
@@ -46,11 +78,18 @@ export class DemoComponent implements OnInit {
     if(typeof e === 'string'){
       this.date = e;
     }
+    this.cdr.detectChanges();
 
     // this.ngZone.runOutsideAngular(() => {
     //   this.date = e;
     // });
     // this.cdRef.detectChanges();
+  }
+
+  ngOnDestroy(): void {
+    if(this.countdownTimerSubscription){
+      this.stopCountdownTime();
+    }
   }
 
 }
